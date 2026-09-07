bf4f348e25c48d6b349a692621c093a23eed3df8
caiomartinscastro <caiomartinscastro1@gmail.com>
2026-09-07 14:38:28 -0300

    Updating

--- App/Program.cs
+++ App/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("ReactPolicy", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
--- App/obj/Debug/net10.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net10.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccb5a4a079646526bdf8db2824618de1cb4fb9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+276d88545a8e30840b2cf12205cac398ec859533")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
